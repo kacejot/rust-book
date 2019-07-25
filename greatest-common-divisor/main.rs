0ed2d75cbb9af58c1fb6b3ffb153f54a4cb1d85a
kacejot <kacejot@fex.net>
2019-07-26 01:39:45 +0300

    Added cmd args handling

--- greatest-common-divisor/main.rs
+++ greatest-common-divisor/main.rs
@@ -1,14 +1,26 @@
+use std::str::FromStr;
 mod gcd;
 
-fn main () {
-	let numbers = vec![14, 28, 35];
+fn parse_args() -> Vec<i64> {
+    let mut numbers = Vec::new();
+	for arg in std::env::args().skip(1) {
+        numbers.push(i64::from_str(&arg)
+			.expect("Error passing arg"));
+	}
 
+    numbers
+}
+
+fn main() {
+	let numbers = parse_args();
 	if numbers.len() == 0 {
-
+        // TODO
 	}
 
 	let mut result = numbers[0];
 	for number in &numbers[1..] {
 		result = gcd::greatest_common_divisor(result, *number);
 	}
+
+	println!("The greatest common divisor for {:?} is {}", numbers, result);
 }
