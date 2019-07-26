56d407080b0be3bc5807a830ae23f84b36c4aac9
kacejot <kacejot@fex.net>
2019-07-27 00:36:25 +0300

    Finished gcd program

--- greatest-common-divisor/main.rs
+++ greatest-common-divisor/main.rs
@@ -2,25 +2,23 @@ use std::str::FromStr;
 mod gcd;
 
 fn parse_args() -> Vec<i64> {
-    let mut numbers = Vec::new();
+	let mut numbers = Vec::new();
 	for arg in std::env::args().skip(1) {
-        numbers.push(i64::from_str(&arg)
+		numbers.push(i64::from_str(&arg)
 			.expect("Error passing arg"));
 	}
-
-    numbers
+	numbers
 }
 
 fn main() {
 	let numbers = parse_args();
 	if numbers.len() == 0 {
-        // TODO
+		println!("Usage: gcd NUMBER ...");
+		std::process::exit(1);
 	}
-
 	let mut result = numbers[0];
 	for number in &numbers[1..] {
 		result = gcd::greatest_common_divisor(result, *number);
 	}
-
 	println!("The greatest common divisor for {:?} is {}", numbers, result);
 }
