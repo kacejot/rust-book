28791a78bb7c5a5f0fc2e52996695021d2ce0e5c
kacejot <kacejot@fex.net>
2019-08-02 01:07:04 +0300

    Refactored logic that seeks maximum of floats

--- procedural-terrain-generation/src/terrain/assessment.rs
+++ procedural-terrain-generation/src/terrain/assessment.rs
@@ -1,7 +1,7 @@
 extern crate num;
 
 use std::ops::BitAnd;
-use num::Num;
+use num::{Num, Float};
 
 /*
 From this paper: http://web.mit.edu/cesium/Public/terrain.pdf
@@ -30,10 +30,14 @@ impl HeightMap {
 		})
 	}
 
+	// TODO: add wrapping_at
+	// TODO: check if x and y are out of bounds
 	pub fn at(&self, x: usize, y: usize) -> f64 {
 		self.map[self.side_size * y + x]
 	}
 
+	// TODO: add wrapping_at_mut
+	// TODO: check if x and y are out of bounds
 	pub fn at_mut(&mut self, x: usize, y: usize) -> &mut f64 {
 		&mut self.map[self.side_size * y + x]
 	}
@@ -43,21 +47,30 @@ pub fn build_slope_map(heigth_map: &HeightMap) {
 	let mut slope_map = HeightMap::with_side_size(heigth_map.side_size).unwrap();
 	for i in 0..heigth_map.side_size {
 		for j in 0..heigth_map.side_size {
-			let slopes =[
+			let slopes = [
 				num::abs(heigth_map.at(i, j) - heigth_map.at(i - 1, j)),
 				num::abs(heigth_map.at(i, j) - heigth_map.at(i + 1, j)),
 				num::abs(heigth_map.at(i, j) - heigth_map.at(i, j - 1)),
 				num::abs(heigth_map.at(i, j) - heigth_map.at(i, j + 1)),
 			];
-			let max_slope = slopes.iter()
-				.max_by(|a, b| a.partial_cmp(b).unwrap())
-					.unwrap();
-			*slope_map.at_mut(i, j) = *max_slope;
+			let slope_max = floats_max(&slopes).unwrap();
+			*slope_map.at_mut(i, j) = *slope_max;
 		}
 	}
 }
 
+// TODO: floats_max must be able to process NaN values
+// TODO: move floats_max to common::num module
+fn floats_max<T>(slice: &[T]) -> Option<&T>
+where T: Float {
+	if slice.len() == 0 {
+		return None;
+	}
+	slice.iter().max_by(|a, b| a.partial_cmp(b).unwrap())
+}
+
+// TODO: is_power_of_two to common::num module
 fn is_power_of_two<T>(number: T) -> bool 
 where T: Num + Copy + BitAnd<Output = T> {
-	return (number & (number - T::one())) == T::zero()
+	(number & (number - T::one())) == T::zero()
 }
