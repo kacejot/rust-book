5242a214d75fa3ea5ca5ae569278f8593e9028c9
Maxim Goncharenko <goncharenko.maxim@apriorit.com>
2019-09-12 13:52:57 +0300

    refactor score module

--- procedural-terrain-generation/src/main.rs
+++ procedural-terrain-generation/src/main.rs
@@ -1,5 +1,8 @@
 mod terrain;
+use terrain::height_map::HeightMap;
+use terrain::score::*;
 
 fn main() {
-	let h = terrain::score::HeightMap::with_edge_size(511);
+	let h = HeightMap::with_edge_size(511);
+	let _sm = build_slope_map(&h);
 }
---
+++ procedural-terrain-generation/src/terrain/height_map.rs
@@ -0,0 +1,41 @@
+pub struct HeightMap {
+    buffer: Vec<f32>,
+    edge_size: usize
+}
+
+impl HeightMap {
+    pub fn with_edge_size(edge_size: usize) -> Self {
+        let edge_size = edge_size.next_power_of_two();
+        println!("creating height map with egde size = {}", edge_size);
+        HeightMap{
+			buffer: vec![0f32; edge_size * edge_size],
+			edge_size: edge_size
+		}
+    }
+
+    pub fn at(&self, x: usize, y: usize) -> f32 {
+		self.buffer[self.edge_size * y + x]
+	}
+
+    pub fn wrapping_at(&self, x: isize, y: isize) -> f32 {
+        let x = wrap(x, self.edge_size);
+        let y = wrap(y, self.edge_size);
+        self.at(x, y)
+    }
+
+	pub fn at_mut(&mut self, x: usize, y: usize) -> &mut f32 {
+		&mut self.buffer[self.edge_size * y + x]
+	}
+
+    pub fn edge_size(&self) -> usize {
+        self.edge_size
+    }
+}
+
+fn wrap(number: isize, edge_size: usize) -> usize {
+    let mut number = number % edge_size as isize;
+    if number < 0 {
+        number = number + edge_size as isize;
+    }
+    number as usize
+}
